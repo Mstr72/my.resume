eddb6b382700ba320d512b2e41fc8fe65877a83e
mstr72 <sky.towermilad@gmail.com>
2019-09-05 15:59:27 +0430

    Updates

---
+++ precache-manifest.073082c1b6c7367f5e7510bd81532b6a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4efcbee1a1fb75cfa1a370aade585783",
+    "url": "/my.resume/index.html"
+  },
+  {
+    "revision": "c97fc7b68a28d7b32fe7",
+    "url": "/my.resume/static/css/main.9496b2c9.chunk.css"
+  },
+  {
+    "revision": "e97c169e851b9654b1dc",
+    "url": "/my.resume/static/js/2.39c5f2bc.chunk.js"
+  },
+  {
+    "revision": "c97fc7b68a28d7b32fe7",
+    "url": "/my.resume/static/js/main.da797c5b.chunk.js"
+  },
+  {
+    "revision": "9ca4a825e8f3bfe5c2af",
+    "url": "/my.resume/static/js/runtime~main.83f7042c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my.resume/static/media/logo.5d5d9eef.svg"
+  }
+]);
